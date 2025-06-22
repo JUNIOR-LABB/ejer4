e2817d7e3149f0d1822081196460bc90208cb9b5
JUNIOR-LAB <72562715@idexperujapon.edu.pe>
2025-06-22 00:25:05 -0500

    Agregar archivos de proyecto.

---
+++ ejer4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, solu;
+            a = 10;
+            b = 3;
+            c = 2;
+            solu = (a + b) * c;
+            Console.WriteLine($"Resultado de la Operación conveninada de ({a} + {b}) * {c} es: " +solu);
+        }
+    }
+}
